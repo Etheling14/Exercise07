52ed854e29fd995d4bc3168aad0861af93a45cfb
Urrea <Surrea1411@gmail.com>
2023-09-09 14:11:14 -0500

    xxx

--- Exercise7/Program.cs
+++ Exercise7/Program.cs
@@ -4,26 +4,48 @@ int numberscredits;
 decimal ValueCredits;
 decimal ValueTuition;
 decimal ValueSubsidy;
-
+Console.BackgroundColor = ConsoleColor.DarkCyan;
+Console.Clear();
 do
 {
-    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine("-------------------------------------------------------------------------------");
     Console.ForegroundColor = ConsoleColor.White;
-    RequestData(out stratum, out numberscredits, out ValueCredits);
+    stratum = RequestInt("Enter the stratum: ");
+    numberscredits = RequestInt("Enter the numbers credits: ");
+    ValueCredits = RequestDecimal("Enter the value credist: ");
     ValueTuition = CalculateTuition(stratum, numberscredits, ValueCredits);
     ValueSubsidy = CalculateSubsidy(stratum);
     ShowResults(ValueTuition, ValueSubsidy);
 }while(true);   
 
-static void RequestData(out int stratum, out int numbersCredits, out decimal valueCredits)
+static int RequestInt(string message)
 {
-    Console.WriteLine($"Enter the stratum..............");
-    stratum = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine($"Enter the number credits.......");
-    numbersCredits = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine($"Enter the value credits.........");
-    valueCredits = Convert.ToDecimal(Console.ReadLine());
+    Console.Write(message);
+    var inputString = Console.ReadLine();   
+    try
+    {
+        return Convert.ToInt32(inputString);
+    }
+    catch (Exception)
+    {
+        throw new Exception("You must enter a valid integer number.");
+    }
+    
+}
+
+static decimal RequestDecimal(string message)
+{
+    Console.Write(message);
+    var inputDecimal = Console.ReadLine();
+    try
+    {
+        return Convert.ToDecimal(inputDecimal);
+    }
+    catch (Exception)
+    {
+        throw new Exception("You must enter a valid integer number.");
+    }
 }
 
 static decimal CalculateTuition(int stratum, int numbersCredits, decimal valueCredits)
@@ -55,6 +77,9 @@ static void ShowResults(decimal ValueTuition, decimal ValueSubsidy)
 {
     Console.WriteLine($"The value of the tuition is: {ValueTuition:C2}");
     Console.WriteLine($"The value of the Subsidy is: {ValueSubsidy:C2}");
-    Console.WriteLine("Press enter to calculate another salary or Ctrl + C to finish.");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"----------------------------------------------");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"Press enter to calculate another salary or Ctrl + C to finish.");
     Console.ReadKey();
 }
